ebcbf2dc5f9db094c18a5e78f1e286eb26709257
Param Factory <factory@sociobot.in>
2026-08-28 17:27:26 +0000

    sync package metadata with v0.1.1 release

--- packaging/homebrew/sideload-readiness.rb
+++ packaging/homebrew/sideload-readiness.rb
@@ -1,17 +1,17 @@
 class SideloadReadiness < Formula
   desc "Read-only Android sideload readiness checks"
   homepage "https://sideload-readiness.sociobot.in"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_arm do
-    url "https://github.com/B-Divyesh/sf-sideload-readiness/releases/download/v0.1.0/sideload-readiness-macos-aarch64.tar.gz"
-    sha256 "d0878a02078dee136d8eb20e17917290945cadcf0578b9ea9e8a8e86454e3a21"
+    url "https://github.com/B-Divyesh/sf-sideload-readiness/releases/download/v0.1.1/sideload-readiness-macos-aarch64.tar.gz"
+    sha256 "e999964dbe1f06631c341091ad215bdffb907e98d634d9095783babdf32f2fe8"
   end
 
   on_intel do
-    url "https://github.com/B-Divyesh/sf-sideload-readiness/releases/download/v0.1.0/sideload-readiness-macos-x86_64.tar.gz"
-    sha256 "1dd5054bc66fae60ac9354fc0a74167d94ed1bd5a1522326359396e556b37867"
+    url "https://github.com/B-Divyesh/sf-sideload-readiness/releases/download/v0.1.1/sideload-readiness-macos-x86_64.tar.gz"
+    sha256 "c9b8e7bcaca39c3e8b0c4877d5872526e18ba98855be24c574ff4fb089730bf2"
   end
 
   def install
